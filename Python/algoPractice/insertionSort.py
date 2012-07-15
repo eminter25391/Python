7bcba49d2f3278b6ba1b90dd40b010ac6c7149a8
Luu Tuan Hai <eminter25391@gmail.com>
2012-07-14 19:06:00 -0500

    First commit

---
+++ Python/algoPractice/insertionSort.py
@@ -0,0 +1,10 @@
+a = [5, 6, 1, 9, 4, 3, 10]
+print 'Original sequence: ', a
+for j in range(1, len(a)):
+    key = a[j]
+    i = j - 1
+    while i >= 0 and a[i] > key:
+        a[i+1] = a[i]
+        i -= 1
+    a[i+1] = key
+print 'Sorted sequence: ', a
